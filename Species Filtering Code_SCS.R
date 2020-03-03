ac23bc62d1bddff2fc35361554f34b9ece9e59d3
silknets <silknets@vt.edu>
2020-03-03 10:01:36 -0500

    Major aesthetic revisions after 02March2020 meeting with MCM.

--- Species Filtering Code_SCS.R
+++ Species Filtering Code_SCS.R
@@ -1,5 +1,5 @@
 # ALB / SCS revised version of the National Fishes Vulnerability Assessment "GBIF data filtering and AOO estimation_11_13_2018.R"
-# Sam Silknetter 4Feb2020
+# Sam Silknetter 02March2020
 
 # install necessary libraries
 ###### SCS to remove unnecessary libraries
@@ -22,107 +22,116 @@ library(ggmap)
 library(rgeos)
 library(geosphere)
 
-# Set data paths
-PATH_NatureServeFishRanges <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/NatureServe Shapefiles - All species"
-PATH_ESTUARY <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/merged estuary"
+# Set data paths for spatial data. All avaiable through https://github.com/silknets/National-RCS 
+PATH_NatureServeFishRanges <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/NatureServe Shapefiles - All species" 
+PATH_Estuary <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/merged estuary"
 PATH_StudyExtent <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/Region Shapefiles"
 
-# Set spatial CRS
+# Set a local path to save CSV outputs in this script. 
+PATH_SpeciesData <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/Species_Data/" 
+#PATH_East <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/Species_Data/East/"
+#PATH_Miss <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/Species_Data/Miss/"
+#PATH_Gulf <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/Species_Data/Gulf/"
+#PATH_West <- "G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/Species_Data/West/"
+
+# Set spatial coordinate reference system (CRS)
 crs.geo <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
 crs.albers <- CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=km +no_defs")
 
-# Read in fixed data
-estuary <- readOGR(dsn = PATH_ESTUARY, layer="Merged_estuaries")
-proj4string(estuary) <- crs.geo # Warning expected - spTransform used next to address
+# Read in fixed spatial data. See Table S[X] in Silknetter et al. for additional details on spatial data.  
+estuary <- readOGR(dsn = PATH_Estuary, layer="Merged_estuaries")
+proj4string(estuary) <- crs.geo # Warning expected using this function - spTransform() used in subsequent line to address.
 reproj_estuary <- spTransform(estuary, crs.geo)
 
-study_extent <- readOGR(dsn = PATH_StudyExtent, layer = "Study Extent") # Limit occurences to those within the Study Extent shapefile
+study_extent <- readOGR(dsn = PATH_StudyExtent, layer = "Study_Extent") # Limit occurences to those within the Study Extent shapefile.
 reproj_study_extent <- spTransform(study_extent, crs.geo)
 
-region_east <- readOGR(dsn = PATH_StudyExtent, layer = "East") # Limit occurences to those within the East Region shapefile
+region_east <- readOGR(dsn = PATH_StudyExtent, layer = "East") # Limit occurences to those within the East Region shapefile.
 reproj_region_east <- spTransform(region_east, crs.geo)
 
-region_miss <- readOGR(dsn = PATH_StudyExtent, layer = "Mississippi") # Limit occurences to those within the Mississippi Region shapefile
+region_miss <- readOGR(dsn = PATH_StudyExtent, layer = "Mississippi") # Limit occurences to those within the Mississippi Region shapefile.
 reproj_region_miss <- spTransform(region_miss, crs.geo)
 
-region_gulf <- readOGR(dsn = PATH_StudyExtent, layer = "Gulf") # Limit occurences to those within the Gulf Region shapefile
+region_gulf <- readOGR(dsn = PATH_StudyExtent, layer = "Gulf") # Limit occurences to those within the Gulf Region shapefile.
 reproj_region_gulf <- spTransform(region_gulf, crs.geo)
 
-region_west <- readOGR(dsn = PATH_StudyExtent, layer = "West") # Limit occurences to those within the West Region shapefile
+region_west <- readOGR(dsn = PATH_StudyExtent, layer = "West") # Limit occurences to those within the West Region shapefile.
 reproj_region_west <- spTransform(region_west, crs.geo)
 
-# Import candidate species list, which can be found in the 
-# NFVAP - National RCS Google Drive Space in the Species Selection Folder: "MergedSpeciesList_2020.01.16alb.csv".
-SpeciesList <- read_csv("G:/Shared drives/NFVAP - National RCS/Species Selection/MergedSpeciesList_2020.01.27.csv")
+# Import candidate species list (N=159 species)
+SpeciesList <- read_csv("G:/Shared drives/NFVAP - National RCS/R Code/National-RCS/CandidateSpeciesList_2020.03.02.csv") 
 
-# Create an empty data frame to be populated with occurrence point data by filtering step
-Filters <- data.frame(matrix(vector(), 0, 10, dimnames=list(c(), c("Species", "All_Data", "GBIF_Issues", 
+# Create an empty data frame to be populated with occurrence points after each numbered step (N=10).
+ColumnCount <- 10 # The number of columns for the Filter dataframe. Must match the number of 'dimnames' strings entered below. 
+Filters <- data.frame(matrix(vector(), 0, ColumnCount, dimnames=list(c(), c("Species", "All_Data", "GBIF_Issues", 
     "Study_Extent", "Estuary", "NatureServe", "East", "Mississippi", "Gulf", "West"))), stringsAsFactors=F)
 
-# Pull global occurrence data for all species in candidate list. Limit=199k, the max for this function
+# Pull global occurrence data for all species in candidate list. 
+RecordLimit = 199000 # This is the maximum number of occurrence points/species allowed by the occsearch() function.
+
 for (i in 1:nrow(SpeciesList)){
   
 # Using the scientificName as the search parameter means we'll get back any records for synonyms (ex: searching "Ameiurus
-# serracanthus" brings back records for that as well as "Ictalurus serracanthus")
-GBIFSpecies <- occ_search(scientificName = SpeciesList[i,]$SciName, hasCoordinate = T, limit = 199000, fields = c("species", "decimalLatitude",
+# serracanthus" brings back records for that as well as "Ictalurus serracanthus").
+GBIFSpecies <- occ_search(scientificName = SpeciesList[i,]$SciName, hasCoordinate = T, limit = RecordLimit, fields = c("species", "decimalLatitude",
     "decimalLongitude", "issues", "eventDate", "geodeticDatum","datasetName", "occurrenceID", "occurrenceStatus", "country", "countryCode", 
     "scientificName", "locality", "dataset", "catalogNumber", "institutionCode"))
-GBIFSpeciesData <- as.data.frame(GBIFSpecies$data) #Save search records as a dataframe
+GBIFSpeciesData <- as.data.frame(GBIFSpecies$data) #Save GBIF search records as a dataframe.
 
-#1--  Add species name (Species) and the number of global occurrences in GBIF (All_Data) to 'Filters' dataframe
-Filters[i,1]  <- as.character(GBIFSpeciesData$species[1]) # Save species name in column 1 of row [i] 
-Filters[i,2] <- as.numeric(nrow(GBIFSpeciesData)) # Save the number of global occurrence points in column 2 of row [i]
+# Column 1 - Species: Add species name (Species) to 'Filters' dataframe.
+Filters[i,1]  <- as.character(GBIFSpeciesData$species[1]) 
 
-#2-- Filter records that have the gbif issues COUNTRY_COORDINATE_MISMATCH, RECORDED_DATE_MISMATCH, ZERO_COORDINATE, and
+# Column 2 - All_Data: Add the number of global occurrence points in GBIF to 'Filters' dataframe.
+Filters[i,2] <- as.numeric(nrow(GBIFSpeciesData)) 
+
+# Column 3 - GBIF_Issues: Filter records that have the gbif issues COUNTRY_COORDINATE_MISMATCH, RECORDED_DATE_MISMATCH, ZERO_COORDINATE, and
 # IDENTIFIED_DATE_UNLIKELY. 
 dat <- GBIFSpeciesData %>%
-  occ_issues(-cucdmis, -rdatm, -zerocd, -iddatunl) # By using the minus sign [-], entries with these issues are removed from global data
-dat_coord <- dat[c("decimalLongitude", "decimalLatitude")] # Create data frame of longitude and latitude for all valid entries 
-datfsp <- SpatialPointsDataFrame(dat_coord, dat, proj4string = crs.geo) # Save spatial dataframe with correct projection
-# Save # of occurrence points after filtering for GBIF Issues
-Filters[i,3] <- nrow(datfsp)
+  occ_issues(-cucdmis, -rdatm, -zerocd, -iddatunl) # By using the minus sign [-], entries with these issues are removed from global data.
+dat_coord <- dat[c("decimalLongitude", "decimalLatitude")] # Create data frame of longitude and latitude for all valid entries.
+Filters[i,3] <- nrow(dat_coord)
 
-#3-- Filter occurrences that are outside the study extent (within BOTH conterminous US and 4x delineated regions)
-clipped_occurrences_by_extent <- datfsp[reproj_study_extent,]
-# Save # of occurrence points after filtering for Study Extent
-Filters[i,4] <- nrow(clipped_occurrences_by_extent)
+# Column 4 - Study_Extent: Filter occurrences that are outside the study extent (conterminous United States).
+extent <- SpatialPointsDataFrame(dat_coord, dat, proj4string = crs.geo) # Save spatial dataframe with correct projection.
+extent_clipped <- extent[reproj_study_extent,]
+Filters[i,4] <- nrow(extent_clipped)
 
-#4-- Filter out estuarine occurrence points. 
-# Estuary shapefile was downloaded from the EPA website https://www.epa.gov/hesc/estuary-data-mapper-edm 
-# and the Atlantic, Pacific, and Gulf of Mexico estuaries were merged into a single layer.
-sub_est <- clipped_occurrences_by_extent[reproj_estuary,] #subsets occurrence points to those of estuary
-est_pts <- as.data.frame(sub_est) #changes spatial points to a dataframe for mapping with ggplot2
-finalpoints_sp <- clipped_occurrences_by_extent[(!clipped_occurrences_by_extent$occurrenceID %in% est_pts$occurrenceID),] #takes out occurrence records in estuaries
-# Saving # occurrence point data after filtering points in EPA-mapped estuaries
-Filters[i,5] <- nrow(finalpoints_sp)
+# Column 5 - Estuary: Filter out estuarine occurrence points. 
+estuary_subset <- extent_clipped[reproj_estuary,] #Subsets occurrence points to those of estuary.
+estuary_pts <- as.data.frame(estuary_subset) #Changes spatial points to a dataframe.
+estuary_final <- extent_clipped[(!extent_clipped$occurrenceID %in% estuary_pts$occurrenceID),] #Removes occurrence records in estuaries.
+Filters[i,5] <- nrow(estuary_final)
 
-#5-- Filter out occurrences that are outside the native range for the species as determined by NatureServe. Data were downloaded
-# from here http://www.natureserve.org/conservation-tools/data-maps-tools/digital-distribution-native-us-fishes-watershed
-dat_sp_range <- readOGR(dsn = PATH_NatureServeFishRanges, layer = paste(SpeciesList[i,]$Genus, SpeciesList[i,]$Species, sep = "_"))
-proj4string(dat_sp_range) <- CRS("+init=epsg:4269 +proj=longlat +ellps=GRS80 +datum=NAD83")
-reproj_dat_sp_range <- spTransform(dat_sp_range, crs.geo)
-clipped_occurrences_by_range <- finalpoints_sp[reproj_dat_sp_range,]
-# Saving occurrence point data after filtering records outside of a species' native range
-Filters[i,6] <- nrow(clipped_occurrences_by_range)
+# Column 6 - NatureServe: Filter out occurrences that are outside the native range for the species as determined by NatureServe.
+range <- readOGR(dsn = PATH_NatureServeFishRanges, layer = paste(SpeciesList[i,]$Genus, SpeciesList[i,]$Species, sep = "_"))
+proj4string(range) <- CRS("+init=epsg:4269 +proj=longlat +ellps=GRS80 +datum=NAD83")
+reproj_range <- spTransform(range, crs.geo)
+range_clipped <- estuary_final[reproj_range,]  
+Filters[i,6] <- nrow(range_clipped) #This is the final step to filter occurence point data. Columns 7-10 just clip this data by region.
 
-#6-- Clip occurence points to include only those present in the East Region
-occ_east <- datfsp[reproj_region_east,]
-# Save # of occurrence points after filtering for Study Extent
+# Save filtered occurrence points to the SpeciesData path for downstream analyses.
+species <- range_clipped$species[1] #Identify the species name for CSV output. 
+write.csv(range_clipped, file = paste0(PATH_SpeciesData, species, ".csv"), row.names=FALSE)
+
+# Column 7 - East: Clip occurence points to include only those present in the East Region.
+occ_east <- range_clipped[reproj_region_east,]
 Filters[i,7] <- nrow(occ_east)
+#write.csv(occ_east, file = paste0(PATH_East, species, ".csv"), row.names=FALSE)
 
-#7-- Clip occurence points to include only those present in the Mississippi Region
-occ_miss <- datfsp[reproj_region_miss,]
-# Save # of occurrence points after filtering for Study Extent
+# Column 8 - Miss: Clip occurence points to include only those present in the Mississippi Region.
+occ_miss <- range_clipped[reproj_region_miss,]
 Filters[i,8] <- nrow(occ_miss)
+#write.csv(occ_miss, file = paste0(PATH_Miss, species, ".csv"), row.names=FALSE)
 
-#8-- Clip occurence points to include only those present in the East Region
-occ_gulf <- datfsp[reproj_region_gulf,]
-# Save # of occurrence points after filtering for Study Extent
+# Column 9 - Gulf: Clip occurence points to include only those present in the Gulf Region.
+occ_gulf <- range_clipped[reproj_region_gulf,]
 Filters[i,9] <- nrow(occ_gulf)
+#write.csv(occ_gulf, file = paste0(PATH_Gulf, species, ".csv"), row.names=FALSE)
 
-#9-- Clip occurence points to include only those present in the East Region
-occ_west <- datfsp[reproj_region_west,]
-# Save # of occurrence points after filtering for Study Extent
+# Column 10 - West: Clip occurence points to include only those present in the West Region.
+occ_west <- range_clipped[reproj_region_west,]
 Filters[i,10] <- nrow(occ_west)
+#write.csv(occ_west, file = paste0(PATH_West, species, ".csv"), row.names=FALSE)
 }
-write.csv(Filters, file = "Filters.csv") #output the file as csv
+
+write.csv(Filters, file = "All_Filters_02March2020.csv") #Write a CSV outputfile.
